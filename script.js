4568aaeae4630b1c665710673923e150f1962a04
Sakshipriya23 <23053438@kiit.ac.in>
2025-12-24 13:07:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Web Development Enthusiast", "Passionate Learner"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
